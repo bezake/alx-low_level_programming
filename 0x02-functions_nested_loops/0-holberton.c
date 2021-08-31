8092a5fad8c0815ecc3c2e796fabd257117519c3
bezake <bezawitkendie567@gmail.com>
2021-08-31 14:24:15 -0700

    second commit

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/** 
+ * main - print holberton
+ * Descrioption: using the main function
+ * this program prints "Holberton"
+ * return: 0
+ */
+int main(void)
+{
+	printf("Holberton,\n");
+	return (0);
+}
