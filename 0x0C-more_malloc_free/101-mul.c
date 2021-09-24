147cca8d6365f24481a384009215d2c673ec7f72
bezake <bezawitkendie567@gmail.com>
2021-09-23 23:00:37 -0700

    101 multiple

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,40 +1,74 @@
 #include "main.h"
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
-* string_nconcat - concatenates s1 to the first n bytes of s2
-* @s1: string to be  received
-* @s2: string
-* @n: bytes to be copied from s2
-*
-* Return: pointer to resulting string
-*/
-
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+ * _puts - prints a string, followed by a new line,
+ * @str: pointer to the string to print
+ * Return: void
+ */
+void _puts(char *str)
 {
-char *neow;
-unsigned int a, b, len1, len2;
-
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
-len1 = strlen(s1);
-len2 = strlen(s2);
-if (n >= len2)
-n = len2;
-neow = malloc(sizeof(*neow) * (len1 + n + 1));
-if (neow == NULL)
-return (NULL);
-for (a = 0; s1[a] != '\0'; a++)
-neow[a] = s1[a];
-for (b = 0; b < n && s2[b] != '\0'; b++)
+int i = 0;
+while (str[i])
 {
-neow[a] = s2[b];
-a++;
+_putchar(str[i]);
+i++;
 }
-neow[a] = '\0';
-return (neow);
+}
+/**
+ * _atoi - convert a string to an integer.
+ * @s: char type string
+ * Return: integer converted
+ */
+int _atoi(const char *s)
+{
+int sign = 1;
+unsigned long int resp = 0, firstNum, i;
+for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
+{
+if (s[firstNum] == '-')
+{
+sign *= -1;
+}
+}
+for (i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
+{
+resp *= 10;
+resp += (s[i] - 48);
+}
+return (sign *resp);
+}
+/**
+ * print_int - prints an integer.
+ * @n: int
+ * Return: 0
+ */
+void print_int(unsigned long int n)
+{
+unsigned  long int divisor = 1, i, resp;
+for (i = 0; n / divisor > 9; i++, divisor *= 10)
+;
+for (; divisor >= 1; n %= divisor, divisor /= 10)
+{
+resp = n / divisor;
+_putchar('0' + resp);
+}
+}
+/**
+ * main - print the result of the multiplication, followed by a new line
+ * @argc: int
+ * @argv: list
+ * Return: 0
+ */
+int main(int argc, char const *argv[])
+{
+(void)argc;
+if (argc != 3)
+{
+_puts("Error ");
+exit(98);
+}
+print_int(_atoi(argv[1]) * _atoi(argv[2]));
+_putchar('\n');
+return (0);
 }
