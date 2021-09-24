d7bb1ea292a32da4ccdfbcd191fbd2a2cccb4f3f
bezake <bezawitkendie567@gmail.com>
2021-09-23 22:58:33 -0700

    concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,48 +1,40 @@
-#include <stdlib.h>
 #include "main.h"
-/**
- * _strlen - get length of string.
- * @str: string.
- * Return: length.
- */
-unsigned int _strlen(char *str)
-{
-	unsigned int i;
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-	i = 0;
-	while (str[i] != '\0')
-	{
-		++i;
-	}
-	return (i);
-}
 /**
- * string_nconcat - concatenates two strings.
- * @s1: strging 1.
- * @s2: string 2.
- * @n: byets of s2 to include.
- * Return: pointer to a string or NULL.
- */
+* string_nconcat - concatenates s1 to the first n bytes of s2
+* @s1: string to be  received
+* @s2: string
+* @n: bytes to be copied from s2
+*
+* Return: pointer to resulting string
+*/
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *str;
-	unsigned int len1, len2, i, j;
+char *neow;
+unsigned int a, b, len1, len2;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	len1 = _strlen(s1);
-	len2 = _strlen(s2);
-	if (n >= len2)
-		n = len2;
-	str = malloc(len1 + n + 1 * sizeof(char));
-	if (str == NULL)
-		return (NULL);
-	for (i = 0; i < len1; ++i)
-		str[i] = s1[i];
-	for (j = 0; j < n; ++j, ++i)
-		str[i] = s2[j];
-	str[i] = '\0';
-	return (str);
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+len1 = strlen(s1);
+len2 = strlen(s2);
+if (n >= len2)
+n = len2;
+neow = malloc(sizeof(*neow) * (len1 + n + 1));
+if (neow == NULL)
+return (NULL);
+for (a = 0; s1[a] != '\0'; a++)
+neow[a] = s1[a];
+for (b = 0; b < n && s2[b] != '\0'; b++)
+{
+neow[a] = s2[b];
+a++;
+}
+neow[a] = '\0';
+return (neow);
 }
