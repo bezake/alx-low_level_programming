0291420094dbe8fdc033044976e52224ffe7bb4f
bezake <bezawitkendie567@gmail.com>
2021-09-23 22:41:42 -0700

    multiple

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,30 +1,40 @@
+#include "main.h"
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
-/**
-* main - multiply 2 numbers as arguments
-* @argc: number of arguments
-* @argv: arguments
-* Return: 0 success
-*/
-int main(int argc, char *argv[])
-{
-unsigned long mul;
-int i, j;
-	if (argc != 3)
-	{ printf("Error\n");
-	exit(98); }
-	for (i = 1; i < argc; i++)
-	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (argv[i][j] > 57 || argv[i][j] < 48)
-			{  printf("Error\n");
-			exit(98); }
-		}
 
-	}
-	mul = atol(argv[1]) *  atol(argv[2]);
-	printf("%lu\n", mul);
-return (0);
+/**
+* string_nconcat - concatenates s1 to the first n bytes of s2
+* @s1: string to be  received
+* @s2: string
+* @n: bytes to be copied from s2
+*
+* Return: pointer to resulting string
+*/
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+char *neow;
+unsigned int a, b, len1, len2;
+
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+len1 = strlen(s1);
+len2 = strlen(s2);
+if (n >= len2)
+n = len2;
+neow = malloc(sizeof(*neow) * (len1 + n + 1));
+if (neow == NULL)
+return (NULL);
+for (a = 0; s1[a] != '\0'; a++)
+neow[a] = s1[a];
+for (b = 0; b < n && s2[b] != '\0'; b++)
+{
+neow[a] = s2[b];
+a++;
+}
+neow[a] = '\0';
+return (neow);
 }
